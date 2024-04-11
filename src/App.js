9af2b20955a6e9cb4186d4d27da4d61323db5eeb
Saurav Yadav <ysaurav777@gmail.com>
2024-04-12 00:33:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from './data';
+import './App.scss';
 
-function App() {
+const App = () => {
+    const [selected,setSelected]=useState(null);
+
+    function singlehit(currId) {
+        console.log(currId);
+        setSelected(currId)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-con'>
+        <div className='accor'>
+            {
+                data && data.length>0 ? 
+                data.map(dataItem => <div className='item'>
+                    <div onClick={()=>{singlehit(dataItem.id)}} className='title'>
+                        <div>
+                            <h3>{dataItem.question}</h3>
+                        </div>
+                        <div>
+                            <span className='plussym'>+</span>
+                        </div>
+                    </div>
+                    <div>
+                            {
+                                selected===dataItem.id ? 
+                                <div className='con-answer'>{dataItem.answer}</div>
+                                : null
+                            }
+                        </div>
+                </div>)
+                : <div>No Data Found</div>
+            }
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
