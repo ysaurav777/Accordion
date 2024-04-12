5b91d006abc293336f1dc61acbc15a045b561845
Saurav Yadav <ysaurav777@gmail.com>
2024-04-12 11:24:54 +0530

    new changes"

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import './App.scss';
 
 const App = () => {
     const [selected,setSelected]=useState(null);
+    const [multisel,enablemultisel]=useState(false);
+    const [mulsel,setmulset]=useState([]);
 
     function singlehit(currId) {
-        console.log(currId);
-        setSelected(currId)
+        if(currId===selected) {
+            setSelected(null);
+        }
+        else {
+            setSelected(currId);
+        }
     }
+
+    function multihit(currId) {
+        let ok = [...mulsel];
+        const findindex=ok.indexOf(currId);
+        if(findindex===-1) {
+            ok.push(currId);
+        }
+        else {
+            ok.splice(findindex,1);
+        }
+        console.log(ok);
+        setmulset(ok);
+    }
+
   return (
     <div className='main-con'>
+        <button className='multi-sel' onClick={()=> enablemultisel(!multisel)} >Enable MultiSelection</button>
         <div className='accor'>
             {
                 data && data.length>0 ? 
                 data.map(dataItem => <div className='item'>
-                    <div onClick={()=>{singlehit(dataItem.id)}} className='title'>
+                    <div onClick={multisel?()=>{multihit(dataItem.id)} :()=>{singlehit(dataItem.id)}} className='title'>
                         <div>
                             <h3>{dataItem.question}</h3>
                         </div>
@@ -25,7 +46,7 @@ const App = () => {
                     </div>
                     <div>
                             {
-                                selected===dataItem.id ? 
+                                selected===dataItem.id || mulsel.indexOf(dataItem.id)!==-1 ? 
                                 <div className='con-answer'>{dataItem.answer}</div>
                                 : null
                             }
